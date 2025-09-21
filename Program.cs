e6df43d8e49519c1f41302ceb657ce294048c13e
Christan <cralleo2000@gmail.com>
2025-09-21 21:06:25 +0200

    Initial

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+Console.Write("Enter password: ");
+string password = Console.ReadLine();
+Console.Write("Enter user ID: ");
+uint userId = uint.Parse(Console.ReadLine());
+
+// Outputs
+bool userIsAdmin = userId > 65536;
+bool usernameValid = username.Length >= 3;
+bool passwordContainsSpecial = password.Contains('$') || password.Contains('|') || password.Contains('@');
+bool passwordValid = passwordContainsSpecial &&
+                     ((userIsAdmin && password.Length >= 20) || (!userIsAdmin && password.Length >= 16));
